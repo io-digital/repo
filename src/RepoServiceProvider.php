4f9dbbaa4dd3b8e22c35095692b716d9028517c9
vagrant <vagrant@homestead>
2016-09-06 13:02:58 +0000

    new files copies

--- src/RepoServiceProvider.php
+++ src/RepoServiceProvider.php
@@ -28,14 +28,12 @@ class RepoServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/Models' => app_path('Models')
-        ]);
+            __DIR__.'/Models' => app_path('Models'),
 
-        $this->publishes([
             __DIR__.'/Models/Concrete/_AbstractEloquentRepository.php' => file_exists(app_path('Models/Concrete/AbstractEloquentRepository.php')) ? app_path('Models/Concrete/_AbstractEloquentRepository.php') : app_path('Models/Concrete/AbstractEloquentRepository.php'),
 
             __DIR__.'/Models/Contracts/_RepositoryInterface.php' => file_exists(app_path('Models/Contracts/RepositoryInterface.php')) ? app_path('Models/Contracts/_RepositoryInterface.php') : app_path('Models/Contracts/RepositoryInterface.php')
-        ], 'base-repos');
+        ]);
     }
 
     /**
